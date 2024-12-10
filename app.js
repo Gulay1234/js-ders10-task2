cf8f5fe82eae9f664e51cba84c140e5506e539a0
Gulay <gulaynasibova28@gmail.com>
2024-12-10 15:05:54 +0400

    js-ders10-task2
    js-ders10-task2

---
+++ app.js
@@ -0,0 +1,7 @@
+let counter = prompt("Dörd rəqəmli ədəd daxil edin!");
+if (counter.length === 4) {
+    let a = Number(num[0]) + Number(num[1]) + Number(num[2]) + Number(num[3]);
+    console.log(a);
+} else {
+    console.log("Dörd rəqəmli bir ədəd daxil edin!");
+}
